85e7390ff8694b6a77f9128d4c6073558293d73e
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-03-05 12:52:31 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,10 +14,10 @@ export const Header = () => {
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" id="navbarDropdown" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#!">All Products</a></li>
+                                <li><a className="dropdown-item" href="/">All Products</a></li>
                                 <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a className="dropdown-item" href="#!">New Arrivals</a></li>
+                                <li><a className="dropdown-item" href="/">Popular Items</a></li>
+                                <li><a className="dropdown-item" href="/">New Arrivals</a></li>
                             </ul>
                         </li>
                     </ul>
