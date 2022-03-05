2e1c9635df49e7da016e60db7090e3ca50d70215
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-03-05 12:54:10 +0100

    Update ProductSection.jsx

--- src/components/main/ProductSection.jsx
+++ src/components/main/ProductSection.jsx
@@ -6,7 +6,7 @@ export const Product = () => {
     <section className="py-5">
       <div className="container px-4 px-lg-5 my-5">
         <div className="row gx-4 gx-lg-5 align-items-center">
-        <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src={ProductImg} alt="image product..." /></div>      
+        <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src={ProductImg} alt="product..." /></div>      
             <div className="col-md-6">
                         <div className="small mb-1">SKU: BST-498</div>
                         <h1 className="display-5 fw-bolder">Shop item template</h1>
